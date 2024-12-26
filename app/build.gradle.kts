3a52ca5b2d720f01aaf75af4b28dc51bb1fa417f
demeth0 <samuel.dechambre@gmail.com>
2024-12-26 18:03:33 +0100

    Update dependencies (library...) also update gradle plugin with kotlin language and catalogue version.

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.demeth.massaudioplayer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.demeth.massaudioplayer"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 2
+        versionName = "5.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles( getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.androidx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
